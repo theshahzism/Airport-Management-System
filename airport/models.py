8d8ca87c805271a409e8cf8af1183f9f6edbbcaa
Muhammad-Faizan-Soomro <130318306+Muhammad-Faizan-Soomro@users.noreply.github.com>
2023-07-12 04:33:58 +0500

    tables done

--- airport/models.py
+++ airport/models.py
@@ -1,19 +1,65 @@
 from django.db import models
+from viewflow.fields import CompositeKey
 
-class Person(models.Model):
-    name = models.CharField(max_length=50)
-    birthday = models.DateField()
-    anniversary = models.ForeignKey(
-        "Anniversary", on_delete=models.CASCADE)
-    address = models.ForeignKey(
-        "Address", on_delete=models.CASCADE)
+class AirportPlace(models.Model):
+    placeID = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
 
-class Address(models.Model):
-    line1 = models.CharField(max_length=150)
-    line2 = models.CharField(max_length=150)
-    postalcode = models.CharField(max_length=10)
-    city = models.CharField(max_length=150)
-    country = models.CharField(max_length=150)
+    class Meta:
+        indexes = [models.Index(fields=['placeID'])]
 
-class Anniversary(models.Model):
-    date = models.DateField()
+class Restaurants(models.Model):
+    placeID = models.ForeignKey("AirportPlace", on_delete=models.CASCADE)
+    brand = models.CharField(max_length=50)
+
+class Terminal(models.Model):
+    placeID = models.ForeignKey("AirportPlace", on_delete=models.CASCADE)
+    capacity = models.CharField(max_length=50)
+
+class Department(models.Model):
+    IDno = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=50)
+
+    class Meta:
+        indexes = [models.Index(fields=['IDno'])]
+
+class Security(models.Model):
+    IDno = models.ForeignKey("Department", on_delete=models.CASCADE)
+    secName = models.CharField(max_length=50)
+    grade = models.CharField(max_length=50)
+
+class Staff(models.Model):
+    IDno = models.ForeignKey("Department", on_delete=models.CASCADE)
+    staffName = models.CharField(max_length=50)
+    grade = models.CharField(max_length=50)
+
+class Engineer(models.Model):
+    IDno = models.ForeignKey("Department", on_delete=models.CASCADE)
+    engName = models.CharField(max_length=50)
+    grade = models.CharField(max_length=50)
+
+class AirlineOperates(models.Model):
+    airlineID = models.AutoField(primary_key=True)
+    airlineName = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+
+    class Meta:
+        indexes = [models.Index(fields=['airlineID'])]
+
+class Tickets(models.Model):
+    id = CompositeKey(columns=['airlineName', 'seatNO'])
+    seatNO = models.IntegerField()
+    airlineName = models.CharField(max_length=50)
+    price = models.PositiveBigIntegerField()
+    airlineClass = models.CharField(max_length=50)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departureTime = models.TimeField()
+    arrivalTime = models.TimeField()
+
+class Luggage(models.Model):
+    id = CompositeKey(columns=['airlineName', 'seatNO'])
+    airlineName = models.CharField(max_length=50)    
+    luggageID = models.IntegerField()
+    weight = models.IntegerField()
