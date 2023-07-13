025a470a4b78ca6cb118173baac853a212cf0d8b
Syed Shahzain Hassan <shahzain.hassan.1@gmail.com>
2023-07-13 18:23:05 +0500

    Got the data and displayed it on the first UI page

--- airport/views.py
+++ airport/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import AirlineOperates
+from .models import AirportPlace
+from .models import Restaurants
+from .models import Terminal
+from .models import Department
+from .models import Security
+from .models import Staff
+from .models import Engineer
+from .models import Tickets
+from .models import Tickets_Luggage
 
+from django.db import connection
 
-
+#We have joined two tables from tickets and airline operates to show data on the first home page for schedules
 def homePage(request):
-    return render(request,"index.html")
+    cursor=connection.cursor()
+    cursor.execute('select airlineName,source,destination,departureTime,price from airport_airlineoperates join airport_tickets on airport_airlineoperates.airlineID=airport_tickets.airlineID_id')
+    results=cursor.fetchall()
+    return render(request,"index.html",{'index_results':results})
+
+# def tickets(request):
+#     airline=AirlineOperates.objects.all
+#     return render(request,'tickets.html',{'all':airline})
